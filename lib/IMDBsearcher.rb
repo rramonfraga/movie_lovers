5f47c0beac8eed8708fe9e9e0b817b03398d2326
Rafael Ramon Fraga <rafael_ramon_fraga@yahoo.es>
2015-11-06 09:57:14 +0100

    Fist commit. I created all the stuff for the proyect

---
+++ lib/IMDBsearcher.rb
@@ -0,0 +1,2 @@
+class IMDBsearcher
+end
---
+++ server.rb
@@ -0,0 +1,6 @@
+require 'sinatra'
+require 'sinatra/reloader'
+require 'imdb'
+#require 'pry'
+require_relative './lib/Blog.rb'
+require_relative './lib/Post.rb'
